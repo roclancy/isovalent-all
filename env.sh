a312548adc96509d06444d368945c24299942614
roclancy <roclancy@cisco.com>
2025-07-25 13:49:11 +0100

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,17 @@
+
+#SET YOUR VARIABLES HERE"
+
+echo "CREATING ENV VARIABLES"
+
+export EKS_CREATE_CLUSTER1=replace-this-with-your-eks-cluster-name
+export EKS_CREATE_CLUSTER2=replace-this-with-your-eks-cluster-name
+
+export EKS_CREATE_REGION=replace-this-with-your-aws-region
+export EKS_EC2_KEY=replace-this-with-youu-pem-key-to-access-your-ec2-instances
+export EKS_CREATE_NODE_GRP=replace-this-with-your-node-group-name
+
+export AWS_ACCESS_KEY_ID=replace-this-with-your-aws-access-key
+export AWS_SECRET_ACCESS_KEY=replace-this-with-your-aws-secret-key
+
+export CILIUM_NAME1=replace-this-with-cilium-name-you-want-to-use
+export CILIUM_NAME2=replace-this-with-cilium-name-you-want-to-use
